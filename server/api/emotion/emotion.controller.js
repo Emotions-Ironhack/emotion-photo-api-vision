0cc9853fc133fd42833c4310b83d58e34001958d
Mario Martin <mariomarsan7@gmail.com>
2017-08-10 17:25:40 +0200

    Refactor emotion.controller.js and vision.js

--- server/api/emotion/emotion.controller.js
+++ server/api/emotion/emotion.controller.js
@@ -3,14 +3,18 @@ const Emotion = require("./Emotion");
 const visionService = require('../../config/vision');
 const emotionAux = require('./emotion.aux');
 const upload = require('../../config/multerService');
+const pify = require('pify');
 
 // EMOTIONS USER LIST
-exports.listUserEmotions = (req, res)=> {
-
+exports.listUserEmotions = (req, res) => {
   let userRef = req.params.userId;
 
-Emotion.find({ userRef: userRef }).populate('userRef')
-    .sort({ created_at:-1 })
+  Emotion.find({
+      userRef: userRef
+    }).populate('userRef')
+    .sort({
+      created_at: -1
+    })
     .exec()
     .then(list => {
       res.json(list);
@@ -40,63 +44,31 @@ exports.getEmotion = (req, res) => {
 
 
 // POST to VISION API -> THEN CREATE AND SAVE EMOTION */
-exports.createEmotion = (req, res)=> {
+exports.createEmotion = (req, res) => {
+  let userRef;
+  let imageURL;
 
-  // function uploadTotal() {
-  let requestUpload = new Promise((resolve, reject) => {
-    upload(req, res, function(err) {
-
-      let infoImage = {};
-      if (req.body.userRef) infoImage.userRef = req.body.userRef;
-
-      if (req.file) {
-
-        // check if url is localhost
-        console.log('HOSTNAME: ',req.get('host'));
-        let hostName = req.get('host');
-
-        if (hostName.includes('localhost'))
-          infoImage.url = 'http://marioms.com/scarlet2.jpg';
-        else
-          infoImage.url = `https://${hostName}/uploads/${req.file.filename}`;
-
-        console.log('INFOURLIMAGE:::::',infoImage);
-        resolve(infoImage);
-
-      } else {
-        reject(err => {
-          console.loog('ERROR in Server createEmotion Reject: ',err);
-        });
-      }
-
+  pify(upload)(req, res)
+    .then(function() {
+      if (req.body.userRef) userRef = req.body.userRef;
+      const hostName = req.get('host');
+      imageURL = hostName.includes('localhost') ?
+        'http://marioms.com/scarlet2.jpg' :
+        `https://${hostName}/uploads/${req.file.filename}`;
+      return imageURL;
+    })
+    .then(imgUrl => visionService(imgUrl))
+    .then(objEmotion => {
+      let maxEmotionObj = emotionAux.getMaxEmotion(objEmotion[0].scores);
+      return new Emotion({
+        userRef: userRef,
+        emotions: objEmotion[0].scores,
+        maxEmotion: maxEmotionObj,
+        image_path: imageURL
+      });
+    })
+    .then(newEmotion => emotionAux.saveEmotion(res, newEmotion))
+    .catch(function(err) {
+      res.status(500).json(err.message || err);
     });
-
-  }); // end promise
-
-  // 2 - Call to API Vision
-  let visionPromise = new Promise((resolve, reject) => {
-    // upload image THEN
-    requestUpload.then(infoImage => {
-      let objEmotion = visionService(infoImage.url, infoImage.userRef);
-      resolve(objEmotion);
-    });
-  });
-
-  // 3 - Create new emotion
-  visionPromise.then(objEmotion => {
-
-    let maxEmotionObj = emotionAux.getMaxEmotion(objEmotion[0].scores);
-
-    const newEmotion = new Emotion({
-      userRef: objEmotion.userRef,
-      emotions: objEmotion[0].scores,
-      maxEmotion: maxEmotionObj,
-      image_path: objEmotion.imageURL
-    });
-
-    console.log('NEW EMOTION', newEmotion);
-
-    emotionAux.saveEmotion(res, newEmotion);
-  }).catch(err => console.log('Error visionPromise: ', err));
-
 };
